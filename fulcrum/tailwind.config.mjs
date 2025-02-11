d55368325e2a8228bb042c10366867665d4950e4
Александр <nikol.alex06@mail.ru>
2025-02-11 12:27:25 +0300

    feat: set tailwind.config

--- fulcrum/tailwind.config.mjs
+++ fulcrum/tailwind.config.mjs
@@ -9,9 +9,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'base-blue': "#28A2FF",
+        dark: "#14213F",
+        'dark-blue': "#006FB3",
+        'light-gray': '#F7F7F7',
+        gray: '#7F7F7F',
+        red: '#FF2600',
+        'dark-red': '#D20101'
       },
+      fontFamily: {
+        sans: ['"Montserrat"', 'sans-serif'],
+        title: ['"Unbounded"', 'sans-serif'],
+      },
+      borderRadius: {
+        base: '10px',
+        light: '15px',
+        medium: '20px',
+        large: '25px'
+      },
+      boxShadow: {
+        down: '0 4px 10px rgba(0, 0, 0, 0.05)',
+        up: '0 -4px 10px rgba(0, 0, 0, 0.05)',
+        center: '0 0 4px rgba(0, 0, 0, 0.08)',
+        inner: 'inset 0 0 4px rgba(0, 0, 0, 0.05)'
+      }
     },
   },
   plugins: [],
