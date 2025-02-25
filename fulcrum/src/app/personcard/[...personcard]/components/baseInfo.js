1c09c0151e3866941549ac25449de348e89a46a4
Александр <nikol.alex06@mail.ru>
2025-02-25 09:53:52 +0300

    feat: >> In proccess to create personal crad

---
+++ fulcrum/src/app/personcard/[...personcard]/components/baseInfo.js
@@ -0,0 +1,53 @@
+'use client'
+
+import { BaseButton } from "@/app/components/buttons"
+import { API_HOST } from "@/app/components/host";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function BaseInfo({person}) {
+    function openChat() {
+        return;
+    }
+
+    return(
+        <div className="flex flex-col gap-2.5">
+            <h3 className="font-title text-dark text-3xl">{person.name}</h3>
+            <p className="text-dark text-xl font-title">{person.university}, {person.course}</p>
+            <p className="text-dark text-xl font-title">О себе:</p>
+            <p className="text-base text-dark">{person.about}</p>
+            <div className="flex gap-5">
+                <BaseButton text={'Перейти в чат'} action={openChat}/>
+                <LikePerson id={person.id}/>
+            </div>
+        </div>
+    )
+}
+
+function LikePerson({id}) {
+    let [isLike, setIsLike] = useState();
+
+    async function like() {
+        try {
+            let res = await fetch(`${API_HOST}/person/like`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "http://localhost:3000",
+                },
+                credentials: 'include',
+                body: JSON.stringify({id: id})
+            });
+            if (res.status == 200) setIsLike(!isLike);
+            else throw new Error(res.status);
+        } catch(e) {
+            console.log(e);
+            return;
+        }
+
+    }
+
+    return (
+        <button onClick={like}><Image alt="like" width={24} height={24} src={isLike ? '/notLike.svg' : '/Like.svg'}/></button>
+    )
+}
